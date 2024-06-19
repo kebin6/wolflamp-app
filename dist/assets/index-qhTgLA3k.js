271a9ba43ccf77a387ad46c1bcb82871c2b8cd4c
切菜大叔 <475583819@qq.com>
2024-06-19 21:22:22 +0800

    save

--- dist/assets/index-qhTgLA3k.js
+++
@@ -1,1 +0,0 @@
-import{aL as n}from"./index-CE6aD7Yz.js";const r=e=>n.post({url:"/exchange",data:e}),s=e=>n.post({url:"/exchange/find",data:e}),a=e=>n.post({url:"/exchange/list",data:e});export{a,s as b,r as e};
---
+++ dist/assets/toast-C5qvnhx0.js
@@ -0,0 +1,1 @@
+import{aT as s}from"./index-DBFXL0r3.js";const i=(o="Loading",a=!0)=>{s({message:o,duration:0,forbidClick:a})};export{i as s};
